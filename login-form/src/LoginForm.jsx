e52f0e56d107370098088b9c9c7fda0b6cf09b1b
pragya-codes <pragya21offc@gmail.com>
2024-04-25 13:55:00 +0530

    3.Login Form - username validation added

--- login-form/src/LoginForm.jsx
+++ login-form/src/LoginForm.jsx
@@ -25,6 +25,11 @@ const StyledContainer = styled.div`
 		padding-bottom: 10px;
 		margin: 0 20px;
 	}
+
+	span {
+		color: red;
+		font-size: 0.7rem;
+	}
 `;
 
 function LoginForm() {
@@ -34,6 +39,11 @@ function LoginForm() {
 		password: '',
 	});
 
+	const [errValues, setErrValues] = useState({
+		errUser: '',
+		errEmail: '',
+		errPass: '',
+	});
 	function handleChange(e) {
 		setFormValues({ ...formValues, [e.target.name]: e.target.value }); //took 70% of brain effort
 	}
@@ -41,47 +51,100 @@ function LoginForm() {
 	function handleSubmit(e) {
 		e.preventDefault();
 		validation(formValues);
+		setFormValues({ username: '', email: '', password: '' });
 	}
 
-	function validation() {}
+	function validation(obj) {
+		const { username, email, password } = obj;
+
+		//username validation
+		if (username.length == 0) {
+			setErrValues({ ...errValues, errUser: 'Enter a username!' });
+		} else if (username.length <= 3 || username.length >= 15) {
+			setErrValues({
+				...errValues,
+				errUser: 'Username should be between 3 to 15 characters only',
+			});
+		}
+	}
 	console.log(formValues);
 	return (
 		<>
-			<StyledContainer>
-				<h1>LOGIN</h1>
-				<form onSubmit={handleSubmit}>
-					<div className="item">
-						<label>Username:</label>
-						<input
-							type="text"
-							name="username"
-							value={formValues.username}
-							onChange={handleChange}
-						></input>
-					</div>
-					<div className="item">
-						<label>Email ID:</label>
-						<input
-							type="text"
-							name="email"
-							value={formValues.email}
-							onChange={handleChange}
-						></input>
-					</div>
-					<div className="item">
-						<label>Password:</label>
-						<input
-							type="password"
-							name="password"
-							value={formValues.password}
-							onChange={handleChange}
-						></input>
-					</div>
-					<div>
-						<button>Submit</button>
-					</div>
-				</form>
-			</StyledContainer>
+			{errValues ? (
+				<StyledContainer>
+					<h1>LOGIN</h1>
+					<form onSubmit={handleSubmit}>
+						<div className="item">
+							<label>Username:</label>
+							<input
+								type="text"
+								name="username"
+								value={formValues.username}
+								onChange={handleChange}
+							></input>
+							<span>{errValues.errUser}</span>
+						</div>
+						<div className="item">
+							<label>Email ID:</label>
+							<input
+								type="text"
+								name="email"
+								value={formValues.email}
+								onChange={handleChange}
+							></input>
+							<span>{errValues.errEmail}</span>
+						</div>
+						<div className="item">
+							<label>Password:</label>
+							<input
+								type="password"
+								name="password"
+								value={formValues.password}
+								onChange={handleChange}
+							></input>
+						</div>
+						<div>
+							<button>Submit</button>
+						</div>
+					</form>
+				</StyledContainer>
+			) : (
+				<StyledContainer>
+					<h1>LOGIN</h1>
+					<form onSubmit={handleSubmit}>
+						<div className="item">
+							<label>Username:</label>
+							<input
+								type="text"
+								name="username"
+								value={formValues.username}
+								onChange={handleChange}
+							></input>
+						</div>
+						<div className="item">
+							<label>Email ID:</label>
+							<input
+								type="text"
+								name="email"
+								value={formValues.email}
+								onChange={handleChange}
+							></input>
+						</div>
+						<div className="item">
+							<label>Password:</label>
+							<input
+								type="password"
+								name="password"
+								value={formValues.password}
+								onChange={handleChange}
+							></input>
+						</div>
+						<div>
+							<button>Submit</button>
+						</div>
+					</form>
+				</StyledContainer>
+			)}
 		</>
 	);
 }
